e5d5f9c601a3a3333615bec2d5d4108f913f18e6
Nitin Bhargava <nitinbhargava@nitins-mbp.home>
2017-04-08 00:56:44 -0700

    First Checkin!! Hello world!

---
+++ hello.py
@@ -0,0 +1,10 @@
+print "Hello this is the start of learning python"
+print "Lets see how it is going to work"
+print 'I "said" do not touch this.'
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
